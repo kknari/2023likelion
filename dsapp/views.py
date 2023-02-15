e166953d80ac7bef5d5f4cd1ebb507fc2b023902
jiinkyung <jinkyung010310@gmail.com>
2023-02-16 02:48:05 +0900

    페이지네이션 완료

--- dsapp/views.py
+++ dsapp/views.py
@@ -12,7 +12,7 @@ def talk(request):
     comments = Comment.objects.filter().order_by('-date')
     paginator = Paginator(posts,  3)
     page = request.GET.get('page', None)
-    if page == None or page == "":
+    if page == None or page == " ":
         page = 1
     posts = paginator.page(page)
     return render(request, 'talk.html', {'posts' : posts, 'comments' : comments})
