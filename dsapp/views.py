e534d89e11e86600496037c2a659d22b784fcffc
DAASHeo <hibstar719@naver.com>
2023-02-16 02:38:10 +0900

    오류 수정

--- dsapp/views.py
+++ dsapp/views.py
@@ -10,18 +10,11 @@ def main(request):
 def talk(request):
     posts = Post.objects.filter().order_by('-date')
     comments = Comment.objects.filter().order_by('-date')
-<<<<<<< HEAD
-    paginator = Paginator(posts, 3)
-    page = request.GET.get('page')
-    if not page:
-        page = 1
-=======
     paginator = Paginator(posts,  3)
     page = request.GET.get('page', None)
     if page == None or page == "":
         page = 1
     posts = paginator.page(page)
->>>>>>> b67cd63eafce186c6160889c57105c729926b681
     return render(request, 'talk.html', {'posts' : posts, 'comments' : comments})
 
 def create(request):
